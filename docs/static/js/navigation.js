d9c7de3ce8a82802d8437ea8a85e95633297253f
michaelb <michaelpb@gmail.com>
2023-03-30 15:27:45 -0700

    lots of mobile improvements

---
+++ docs/static/js/navigation.js
@@ -0,0 +1,42 @@
+{
+
+    navigation: [
+        /*{
+            title: "Leadership",
+            file: "index.html",
+        },*/
+        {
+            title: "About",
+            file: "about/",
+        },
+        {
+            title: "Endorsements",
+            file: "endorsements/",
+        },
+        {
+            title: "Contact",
+            file: "contact/",
+        },
+        /*
+        {
+            title: "News",
+            file: "news/",
+        },
+        {
+            title: "Contact",
+            file: "contact/",
+        },
+        */
+    ],
+
+    smallNav: [
+        {
+            href: '/contact/?volunteer=y',
+            title: 'Volunteer',
+        },
+        {
+            href: '/contact/?pledge=y',
+            title: 'Pledge',
+        },
+    ],
+}
