7e818e4ba34212e7e577449cde25e0b1ffbb4a92
michaelb <michaelpb@gmail.com>
2023-07-04 12:58:21 -0700

    Refactor of Endorsements Code; Update of endorsements image

---
+++ src/static/js/endorsements.js
@@ -0,0 +1,34 @@
+{
+    gallery: [
+        {
+            //title: "Mayor of Richmond",
+            //name: "Eduardo Martínez",
+            src: "2306/eduardomartinez.jpg",
+        },
+        {
+            src: "230523/GusNewport4_Square_Endorse.jpg",
+        },
+        {
+            src: "230523/DevinMurphy_Facebook_MadeIntoSquare_512px_2.jpg",
+        },
+        {
+            src: "230523/Paola_Laverde_Square_512px.jpg",
+        },
+        {
+            skip: true,
+            src: "230523/WiP_Square_Endorse_512px.jpg",
+        },
+        {
+            src: "2307/Teamsters_Square_Endorsement.jpg",
+        },
+        {
+            src: "2306/GenovevaCalloway_Endorse__Square.jpg",
+        },
+        {
+            src: "2306/CAMERON_SASAI_Square_ENDORSES.jpg",
+        },
+        {
+            src: "2306/our_revolution_east_bay.jpg",
+        },
+    ],
+}
